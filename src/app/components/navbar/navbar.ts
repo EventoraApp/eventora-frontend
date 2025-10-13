3d583fb4c30f4dee77fb15489058847ae8f53f9d
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-10-13 10:05:37 +0000

    Adjustments and improvements on the landing page

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { provideIcons, NgIcon } from '@ng-icons/core';
-import { ionSearch, ionMenu } from '@ng-icons/ionicons';
+import { ionSearch, ionMenu, ionClose } from '@ng-icons/ionicons';
+import { SideNavbar } from "../side-navbar/side-navbar";
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIcon, RouterLink],
+  imports: [NgIcon, RouterLink, SideNavbar],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
     viewProviders: [
-    provideIcons({ ionSearch,ionMenu }),
+    provideIcons({ ionSearch,ionMenu,ionClose }),
   ],
 })
 export class Navbar {
+  isSideNavActive = false
+
+  onSideBarClose(){
+    this.isSideNavActive = false
+  }
+  
+  onSideBarOpen(){
+    this.isSideNavActive = true
+
+  }
 
 }
