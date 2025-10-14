bf779f6c5aa3d8e576f588d8ea3f3c3291f7f0c0
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-10-14 14:41:58 +0000

    env url

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiURL: 'https://eventora-backend-2.onrender.com/api',
+  appName: 'Eventora (Dev)',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiURL: "https://eventora-backend-2.onrender.com/api",
+    appName: "Eventora"
+};
